49cb335a5948462c004f9a5043f1b0d13ad6f7e0
Altie <altielynn@gmail.com>
2023-11-22 22:23:00 -0700

    add webmanifest extention

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,23 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from '@astrojs/tailwind';
 import mdx from "@astrojs/mdx";
 
+import webmanifest from "astro-webmanifest";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dragonforgenexus.xyz',
   integrations: [
-    // ...
-    sitemap({
-      filter: (page) =>
-        page !== 'https://dragonforgenexus.xyz/DragonForgeGaming' &&
-        page !== 'https://dragonforgenexus.xyz/DRAGONFORGENETWORK',
-    }), tailwind(), mdx()]
+  // ...
+  sitemap({
+    filter: page => page !== 'https://dragonforgenexus.xyz/DragonForgeGaming' && page !== 'https://dragonforgenexus.xyz/DRAGONFORGENETWORK'
+  }), tailwind(), mdx(), webmanifest({
+    name: 'DragonForgeNEXUS',
+    icon: '/public/imgs/Logos/Square/Solid/SVG/DRAGONFORGENEXUS.svg',
+    short_name: 'DFN',
+    description: 'DragonForgeNEXUS',
+    start_url: '/',
+    theme_color: '#3f6212',
+    background_color: '#3f6212',
+    display: 'standalone',
+  }),]
 });
