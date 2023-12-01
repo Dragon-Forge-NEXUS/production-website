fcff4f00386e12baec0e74d880ac965adaa7254c
αltie <77561831+Alties-GamingYT@users.noreply.github.com>
2023-12-01 21:36:09 +0000

    Update `update.bat` DO NOT USE FOR NOW

--- update.bat
+++ update.bat
@@ -1,1 +1,8 @@
-npm i astro@latest | npm i @astrojs/tailwind@latest | npm i @astrojs/sitemap@latest | npm i daisyui@latest | npm i @astrojs/mdx@latest | npm i @astrojs/rss@latest
+:: DO NOT USE THIS FILE, IT WILL ERROR
+:: I have tryed a fixe, please do not use until tested.
+call npm i astro@latest
+call npm i @astrojs/tailwind@latest
+call npm i @astrojs/sitemap@latest
+call npm i daisyui@latest
+call npm i @astrojs/mdx@latest
+call npm i @astrojs/rss@latest
