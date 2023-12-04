9dfff19b10b47c898697461f9e3b269823c0848a
altie <altielynn@gmail.com>
2023-12-03 20:02:58 -0700

    lots of updates

--- update.bat
+++ update.bat
@@ -1,8 +1,1 @@
-:: DO NOT USE THIS FILE, IT WILL ERROR
-:: I have tryed a fixe, please do not use until tested.
-call npm i astro@latest
-call npm i @astrojs/tailwind@latest
-call npm i @astrojs/sitemap@latest
-call npm i daisyui@latest
-call npm i @astrojs/mdx@latest
-call npm i @astrojs/rss@latest
+npm update
